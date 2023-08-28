4f9566fbba5f2dbbabd3dc65ac84de9b5d6eaf21
Jawadhur Raffiwoo <140182146+Jawadhur23@users.noreply.github.com>
2023-08-29 00:02:02 +0530

    Add files via upload

---
+++ Blutooth Beacon/BLE_SERVER/BLE_SERVER.ino
@@ -0,0 +1,30 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  BLEDevice::init("BLEINDOOR");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+
+  pCharacteristic->setValue("IM IN COMMUNICATION LAB ");//its tx text data as location of room or else any place an make change also for user interfacing ....
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined!");
+}
+void loop()
+{
+  // Nothing to add here. We only need the ESP to location BLE.
+}
